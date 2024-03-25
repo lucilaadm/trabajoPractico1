f99b51f773dd2da99b3ad20e3f9a48cd35c227b3
Lucila Amand de Mendieta <amanddemendietalucila@gmail.com>
2024-03-25 14:43:22 +0100

    primer commit ejercicio del 6

--- js/trabajoPractico1.js
+++ js/trabajoPractico1.js
@@ -1,10 +1,18 @@
-let numeroEj10 = parseInt (prompt ("Indique un número"))
 
-document.write ("Su  número fue " + numeroEj10 + "<br>");
+let numeroEj6A = parseInt (prompt ("Indique un número"))
+let numeroEj6B =  parseInt (prompt ("Indique un número")) 
+
+document.write ("Su primer número fue " + numeroEj6A + "<br>" + "Su segundo número fue " + numeroEj6B + "<br>");
 
 
-if (numeroEj10 % 2 === 0 || numeroEj10 % 3 === 0 || numeroEj10 % 5 === 0 || numeroEj10 % 7 === 0) {
-    document.write ("El numero es divisible por 2, 3, 5 o 7")
-    } else {
-    document.write ("El numero no es divisible") }
+
+if (numeroEj6A === numeroEj6B ) {
+    document.write ("Ambos números ingresados son " + numeroEj6A );
+}
+else if (numeroEj6A > numeroEj6B ) {
+    document.write ("El numero mayor es " + numeroEj6A );
+}
+else {
+document.write ("El numero mayor es " + numeroEj6B );
+} 
 
